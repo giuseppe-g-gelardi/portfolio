040d33d5a59db9680bafda0f5c76d09de627664b
n-rdy <giuseppegelardi@icloud.com>
2021-10-27 18:30:37 -0500

    install dracula ui, added personal head shot into directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@dracula/dracula-ui/styles/dracula-ui.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from './components/Navigation'
 import About from './pages/About'
