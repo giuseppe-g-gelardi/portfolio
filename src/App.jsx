84b10b258956868f028fa852f10eb84d9b0cdf28
n-rdy <giuseppegelardi@icloud.com>
2021-10-27 00:34:03 -0500

    add navbar using bootstrap cdn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navigation from './components/Navigation'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
@@ -9,25 +9,17 @@ import Projects from './pages/Projects'
 function App() {
 return(
   <>
-    <Navbar />
-    <h1>hi nebby!</h1>
-
-    <About />
-    <Contact />
-    <Home />
-    <Projects />
-
-    {/* <Router>
-      <Navbar />
+    <Router>
+      <Navigation />
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/about' exact component={About} />
           <Route path='/projects' exact component={Projects} />
           <Route path='/contact' exact component={Contact} />
         </Switch>
-    </Router> */}
+    </Router>
   </>
-)
+  )
 }
 
 export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import { Link } from 'react-router-dom';
+
+export default function Navigation() {
+  return(
+      <nav id="mainNavbar" className="navbar navbar-dark bg-dark navbar-expand-md">
+         <Link to="/" className='navbar-brand'>Hello...</Link>
+
+          <button className="navbar-toggler" 
+                  data-toggle="collapse" 
+                  data-target="#navLinks">
+              <span className="navbar-toggler-icon"></span>
+          </button>
+
+
+          <div class="collapse navbar-collapse" id="navLinks">
+            <ul className="navbar-nav">
+
+              <li className='nav-item'>
+                <Link to='/about' className='nav-link active'>About</Link>
+              </li>
+       
+              <li className='nav-item'>
+                <Link to='/projects' className='nav-link active'>Projects</Link>
+              </li>
+
+              <li className='nav-item'>
+                <Link to='/contact' className='nav-link active'>Contact</Link>
+              </li>
+
+            </ul>
+          </div>
+      </nav>
+  )
+}
+
+
+// // navbar example
+// {/* <nav id="mainNavbar" class="navbar navbar-dark navbar-expand-md py-0 fixed-top">
+//         <a href="#" class="navbar-brand">CANDY</a>
+//         <button class="navbar-toggler" data-toggle="collapse" data-target="#navLinks" aria-label="Toggle navigation">
+//             <span class="navbar-toggler-icon"></span>
+//         </button>
+//         <div class="collapse navbar-collapse" id="navLinks">
+//             <ul class="navbar-nav">
+//                 <li class="nav-item">
+//                     <a href="" class="nav-link">HOME</a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a href="" class="nav-link">ABOUT</a>
+//                 </li>
+//                 <li class="nav-item">
+//                     <a href="" class="nav-link">TICKETS</a>
+//                 </li>
+//             </ul>
+//         </div>
+//     </nav> */}
+
+
+
+
+
+
+// current
+
+//     <nav id="mainNavbar" className='navbar navbar-dark navbar-expand-md bg-dark mb-4'>
+//     <div className='container-fluid'>
+//       <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+//         <Link to='/' className='navbar-brand'>
+//           Hello...
+//         </Link>
+
+//         <ul className='navbar-nav mr-auto'>
+      
+//           <li className='nav-item active'>
+//             <Link to='/about' className='nav-link'>
+//               About
+//             </Link>
+//           </li>
+       
+//           <li className='nav-item active'>
+//             <Link to='/projects' className='nav-link'>
+//               Projects
+//             </Link>
+//           </li>
+
+//           <li className='nav-item active'>
+//             <Link to='/contact' className='nav-link'>
+//               Contact
+//             </Link>
+//           </li>
+      
+//         </ul>
+//       </div>
+//     </div>
+//     </nav>
