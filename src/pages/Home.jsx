dd048841c1d959b0f121fd71cb16ea774cceb307
n-rdy <giuseppegelardi@icloud.com>
2021-10-27 20:47:36 -0500

    start outlining homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import './styles/Home.css'
+import { Heading, Box, Divider, Button } from '@dracula/dracula-ui';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
+
   return(
     <>
-      <div className="container">
-        <div className="row">
-            <div className="col-sm-3"></div>
-            <div className="col-md-6">
-
-
-              <h1 className='heading'>home</h1>
-
-
-
-            </div>
-            <div className="col-md-3"></div>
-        </div>
-      </div>
-  
-
-      
+      <Box id='pageHeading'  width="auto">
+        <Heading color="purpleCyan" size='2xl' align='center'>Hello, World!</Heading> 
+        <Heading color="yellowPink" size='2xl' align='center'>I'm Giuseppe Gelardi   </Heading>
+          <Box mb="sm">
+            <Divider />
+          </Box>
+        <Heading color="pink" size='xl' align='left'>I'm a full-stack software developer</Heading>
+        <Heading color="purple" size='xl' align='right'>And an industrial engineer</Heading>
+        <br />
+        <Button as="a" href="/about" variant="ghost" color="purple" m='sm' size="md" style={{textDecoration: "none"}}>Enter</Button>;
+      </Box>
     </>
   )
 }
