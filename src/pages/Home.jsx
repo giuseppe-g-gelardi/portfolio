f2916529fb50b9d3c1da32eba399c5912bb9e779
n-rdy <giuseppegelardi@icloud.com>
2021-10-27 20:55:30 -0500

    fix front page structure to make more sense

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,15 +8,13 @@ export default function Home() {
   return(
     <>
       <Box id='pageHeading'  width="auto">
-        <Heading color="purpleCyan" size='2xl' align='center'>Hello, World!</Heading> 
-        <Heading color="yellowPink" size='2xl' align='center'>I'm Giuseppe Gelardi   </Heading>
-          <Box mb="sm">
-            <Divider />
-          </Box>
-        <Heading color="pink" size='xl' align='left'>I'm a full-stack software developer</Heading>
-        <Heading color="purple" size='xl' align='right'>And an industrial engineer</Heading>
-        <br />
-        <Button as="a" href="/about" variant="ghost" color="purple" m='sm' size="md" style={{textDecoration: "none"}}>Enter</Button>;
+        <Divider />
+          <Heading color="purpleCyan" size='2xl' align='center'>Hello, World!</Heading> 
+          <Heading color="yellowPink" size='2xl' align='center'>I'm Giuseppe Gelardi   </Heading>
+        <Divider />
+          <Heading color="pink" size='xl' align='left' m="xs" p="sm">I'm a full-stack software developer</Heading>
+          <Heading color="purple" size='xl' align='right' p="sm">And an industrial engineer</Heading>
+        <Divider/>
       </Box>
     </>
   )
