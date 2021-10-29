b5eec7d2421ea24976122b6c4208ee7ebdd4f0dc
n-rdy <giuseppegelardi@icloud.com>
2021-10-29 16:53:27 -0500

    add content and styles to contact and about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,42 +1,45 @@
 import React from 'react'
+import { Container, Row, Col, Image } from 'react-bootstrap'
+import { Heading, Paragraph } from '@dracula/dracula-ui'
 import portrait from '../images/portrait.jpg'
 import sniperschool from '../images/sniperschool.jpg'
-import { Container, Row, Col, Image } from 'react-bootstrap'
 
 export default function About() {
   return (
-    <Container fluid='md' justify>
-      <Row className='justify-content-center'>
-        <Col>
+    <Container fluid='md' justify >
+      <Row className='justify-content-center' xs={{ order: 'last'}}>
+        <Col >
           <Image src={portrait} fluid />
         </Col>
 
-        <Col>
-          <div id='about-right' className='col-lg-12 text-center'>
-            <h3>Hi! My name is Giuseppe</h3>
+        <Col >
+          
+          <div id='about-right' className='col-lg-12 text-center' justify='center'>
+            <Heading>Hi! My name is Giuseppe</Heading>
             <br />
-            <p>I design high-end custom mechanical keyboards</p>
-            <p>
+            <Paragraph>I design high-end custom mechanical keyboards</Paragraph>
+            <Paragraph>
               Currently I am transitioning to a career in web development and as
               you can see, CSS is the bane of my existence. Good thing I'm still
               really good at designing keyboards! :p
-            </p>
+            </Paragraph>
+
           </div>
         </Col>
       </Row>
-      <Row className='justify-content-center'>
+      <Row className='justify-content-center' xs={{ order: 'first'}}>
         <Col>
           <div id='about-left' className='col-lg-12 text-center'>
-            <h4>Not too long ago though...</h4>
+            <Heading>Not too long ago though...</Heading>
             <br />
-            <p>
+            <Paragraph>
               ... I was in the U.S. Army. I held a few positions like sniper
               team leader, fire-team leader and got to train new solders in
               arctic survival. I was also part of a special recon team that
               everyone was jealous of because we didn't have to shave or wear
               nametags. Although it was fun, I prefer programming and keyboards;
               it's easier on my knees
-            </p>
+            </Paragraph>
           </div>
         </Col>
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Anchor, Heading, Box, Divider } from '@dracula/dracula-ui';
 import { 
   AiFillGithub, 
   AiOutlineInstagram, 
@@ -9,35 +10,69 @@ import {
 export default function Contact() {
   return(
     <>
+    <Box p='md'>
       <ul className='navbar-nav me-auto mb-2 mb-md-0'>
         <li className='nav-item active'>
           <a href='https://github.com/giuseppe-g-gelardi' className='nav-link'>
-            {' '}
-            <AiFillGithub size={70}/>
+            <Anchor 
+              className='nav-link active'
+              color='cyan'
+              hoverColor='purple'>
+               <AiFillGithub style={{textDecoration: "none"}} size={70} />
+            </Anchor>
+            <Heading>Check out my GitHub!</Heading>
+            <Divider />
+
+
+            
           </a>
         </li>
 
         <li className='nav-item active'>
           <a href='https://www.instagram.com/giuseppe.g.gelardi/' className='nav-link'>
-            {' '}
-            <AiOutlineInstagram size={70} />
+            <Anchor 
+              className='nav-link active'
+              color='green'
+              hoverColor='red'>
+               <AiOutlineInstagram style={{textDecoration: "none"}} size={70} />
+            </Anchor>
+            <Heading>Follow me on Instagram!</Heading>
+            <Divider />
+
           </a>
         </li>
 
         <li className='nav-item active'>
           <a href='https://www.linkedin.com/in/giuseppe-gelardi/' className='nav-link'>
-            {' '}
-            <AiOutlineLinkedin size={70} />
+      
+            <Anchor 
+              className='nav-link active'
+              color='orange'
+              hoverColor='yellow'>
+               <AiOutlineLinkedin style={{textDecoration: "none"}} size={70} />
+            </Anchor>
+            <Heading>Add me on linked in!</Heading>
+            <Divider />
+
+            
           </a>
         </li>
 
         <li className='nav-item active'>
           <a href='https://www.facebook.com/giuseppe.gelardi.1612' className='nav-link'>
-            {' '}
-            <AiFillFacebook size={70} />
+            <Anchor 
+              className='nav-link active'
+              color='pink'
+              hoverColor='white'>
+               <AiFillFacebook style={{textDecoration: "none"}} size={70} />
+            </Anchor>
+            <Heading>I cant believe were still using Facebook</Heading>
+            <Divider />
+
           </a>
         </li>
       </ul>
+      </Box>
     </>
   )
 }
